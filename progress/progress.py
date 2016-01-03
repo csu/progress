153b95453bba5e7b494aad84e5a57e8560ccaada
Christopher Su <christophersu9@gmail.com>
2016-01-03 19:26:44 +0800

    starting

---
+++ progress/progress.py
@@ -0,0 +1,12 @@
+# Progress
+# Christopher Su
+
+class Progress:
+  # data_store_path should be either a file system path
+  # or a SQLAlchemy compatible path (if use_db is enabled)
+  def __init__(self, data_store_path, use_db=False):
+    self.data_store_path = data_store_path
+
+    # use_db to be implemented
+
+  
